78517dd9c1fe522eb689c54d53858162b5439946
Halcyon-77 <kaireptilon@gmail.com>
2021-03-26 07:53:42 +0000

    added the ability to use the sootsack outside of battle, can now view how much ash you have collected

--- include/strings.h
+++ include/strings.h
@@ -442,6 +442,7 @@ extern const u8 gText_CantStoreImportantItems[];
 extern const u8 gText_DepositedVar2Var1s[];
 extern const u8 gText_NoRoomForItems[];
 extern const u8 gText_ThreeDashes[];
+extern const u8 gText_AshQty[];
 
 // party menu text
 extern const u8 gText_PkmnHPRestoredByVar2[];
