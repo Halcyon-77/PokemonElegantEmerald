161b3b51ac3253987eaa89472281acff388dd78f
Halcyon-77 <kaireptilon@gmail.com>
2021-04-02 17:18:03 +0100

    Implemented TM51 Poison Jab

--- src/pokemon.c
+++ src/pokemon.c
@@ -6850,15 +6850,25 @@ u32 CanMonLearnTMHM(struct Pokemon *mon, u8 tm)
     {
         return 0;
     }
-    else if (tm < 32)
+    else if (tm < 31)
     {
         u32 mask = 1 << tm;
         return gTMHMLearnsets[species][0] & mask;
     }
+    else if (tm < 62)
+    {
+        u32 mask = 1 << (tm - 31);
+        return gTMHMLearnsets[species][1] & mask;
+    }
+    else if (tm < 93)
+    {
+        u32 mask = 1 << (tm - 62);
+        return gTMHMLearnsets[species][2] & mask;
+    }
     else
     {
-        u32 mask = 1 << (tm - 32);
-        return gTMHMLearnsets[species][1] & mask;
+        u32 mask = 1 << (tm - 93);
+        return gTMHMLearnsets[species][3] & mask;
     }
 }
 
@@ -6868,15 +6878,25 @@ u32 CanSpeciesLearnTMHM(u16 species, u8 tm)
     {
         return 0;
     }
-    else if (tm < 32)
+    else if (tm < 31)
     {
         u32 mask = 1 << tm;
         return gTMHMLearnsets[species][0] & mask;
     }
+    else if (tm < 62)
+    {
+        u32 mask = 1 << (tm - 31);
+        return gTMHMLearnsets[species][1] & mask;
+    }
+    else if (tm < 93)
+    {
+        u32 mask = 1 << (tm - 62);
+        return gTMHMLearnsets[species][2] & mask;
+    }
     else
     {
-        u32 mask = 1 << (tm - 32);
-        return gTMHMLearnsets[species][1] & mask;
+        u32 mask = 1 << (tm - 93);
+        return gTMHMLearnsets[species][3] & mask;
     }
 }
 
